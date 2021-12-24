3244522bd10a94cdcc18253ad4c688afe50abe7f
Thales Morato <tha.s.morato@gmail.com>
2021-12-24 19:00:22 -0300

    refact: terminal configs with table columns

--- src/config/terminal.js
+++ src/config/terminal.js
@@ -1,9 +1,19 @@
-import chalk from 'chalk';
+import chalk from "chalk";
+import { currencies } from "./currency.js";
 
 // @TODO: Any guesses about what's missing?
 export default {
   table: {
     leftPad: 2,
-    columns: [],
+    columns: [
+      { field: "position", name: chalk.cyan("Position") },
+      {
+        field: "expectation",
+        name: chalk.magenta(`Expectation ${currencies.expectation}`),
+      },
+      { field: "conversion01", name: chalk.green(currencies.options.conversion01) },
+      { field: "conversion02", name: chalk.red(currencies.options.conversion02) },
+      { field: "conversion03", name: chalk.white(currencies.options.conversion03) },
+    ],
   },
 };
