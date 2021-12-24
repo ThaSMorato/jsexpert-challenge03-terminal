42124f1dd1a9f1df1ab03cdecc6836cb14bdcfc3
Thales Morato <tha.s.morato@gmail.com>
2021-12-24 19:14:23 -0300

    feat: index js with main loop

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 
 import CustomTerminal from "./terminal.js";
 import IncomeService from "./service/IncomeService.js";
+import { currencies } from "./config/currency.js";
 
 const VOCABULARY = {
   STOP: ":q",
@@ -15,11 +16,33 @@ const service = new IncomeService();
 async function mainLoop() {
   console.info("🚀 Running...\n");
   try {
-    // TODO: Looks like you have some work to do right here :)
+    const answer = await terminal.question(
+      `Qual seu cargo e pretensão salarial em ${currencies.expectation}:\nInsira: `
+    );
+
+    if (answer === VOCABULARY.STOP) {
+      terminal.closeTerminal();
+      console.log("Process finished!!");
+      return;
+    }
+
+    const income = await service.generateIncomeFromString(answer);
+    terminal.updateTable(income.format());
+    return mainLoop();
   } catch (error) {
-    // TODO: Don't forget of handling some errors beautifully ;)
+    console.error(`${error.message} Try again :D`);
   }
   return mainLoop();
 }
 
 await mainLoop();
+
+const onStop = () => {
+  terminal.closeTerminal();
+  console.log("Process finished!!");
+
+  //0 tudo certo / 1 erro
+  process.exit(0);
+};
+
+["SIGTERM", "SIGINT"].forEach((event) => process.on(event, onStop));
