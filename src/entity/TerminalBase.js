56018cf1e599c09fdc2bf811286ef8b2b496b2f1
Thales Morato <tha.s.morato@gmail.com>
2022-01-06 09:46:56 -0300

    feat: terminal base class

---
+++ src/entity/TerminalBase.js
@@ -0,0 +1,11 @@
+export class TerminalBase {
+  initialize() {
+    throw new Error("Not implemented");
+  }
+  async question(msg) {
+    throw new Error("Not implemented");
+  }
+  close() {
+    throw new Error("Not implemented");
+  }
+}
